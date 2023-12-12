7a4a6e04a6f6e7b262bb89f442f5ce495ca2da7e
menachem levi <149540975+menithecoder@users.noreply.github.com>
2023-12-12 23:47:21 +0200

    1

--- flask_web.py
+++ flask_web.py
@@ -223,7 +223,7 @@ def convert_jpeg_to_pdf(input_image, output_pdf):
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+                               'favicon.ico', mimetype='image/x-icon')
 
 @app.route('/')
 def index():
